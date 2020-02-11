6ae44cbe1b128a3819e43a68bb942c04018cfe6a
Noah Glaser <glaserpower@gmail.com>
2020-02-11 12:52:25 -0800

    project setup

--- src/App.svelte
+++ src/App.svelte
@@ -1,38 +1,9 @@
 <script>
-  import Player from './Player.svelte';
 </script>
 
-<style>
-  main {
-    width: 80%;
-    padding: 20px;
-    border: solid gray 1px;
-    margin: 0 auto;
-    background-color: wheat;
-    margin: 10vh auto;
-  }
 
-  #player-container {
-    display: flex;
-  }
-
-  button {
-    display: block;
-    width: 100%;
-    margin-top: 20px;
-    border: solid salmon 1px;
-    background-color: sandybrown;
-    color: rgb(61, 56, 56);
-    font-size: 20px;
-    border-radius: 3px;
-  }
-</style>
 
 <main>
-  <h1>Magic The Gathering Counter</h1>
-  <section id="player-container">
-    <Player />
-    <Player />
-  </section>
-  <button>Start Game</button>
+  <h1>Hello World</h1>
+
 </main>
