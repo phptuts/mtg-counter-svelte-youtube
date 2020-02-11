f19dee14a7a3aebae0e9ec9b889a44b5ac901295
Noah Glaser <glaserpower@gmail.com>
2020-02-11 12:48:56 -0800

    Components

---
+++ src/Player.svelte
@@ -0,0 +1,26 @@
+<style>
+  div {
+    flex-grow: 1;
+  }
+  .plus {
+    background-color: seagreen;
+  }
+  .minus {
+    background-color: brown;
+  }
+  button {
+    font-size: 20px;
+    border-radius: 3px;
+    width: 40px;
+    color: white;
+    font-family: monospace;
+    font-weight: bold;
+  }
+</style>
+
+<div>
+  <h2>20</h2>
+  <button class="minus">-</button>
+  <button class="plus">+</button>
+  <h2>Blue Wins</h2>
+</div>
