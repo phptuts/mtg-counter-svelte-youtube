ec4b25c1fd667381dd668def6443a961215af6af
Noah Glaser <glaserpower@gmail.com>
2020-02-11 23:43:26 -0800

    final github pages

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
+import ghPages from 'gh-pages';
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -46,7 +47,9 @@ export default {
 
 		// If we're building for production (npm run build
 		// instead of npm run dev), minify
-		production && terser()
+		production && terser() && ghPages.publish('public', () => {
+			console.log('github pages published')
+		})
 	],
 	watch: {
 		clearScreen: false
